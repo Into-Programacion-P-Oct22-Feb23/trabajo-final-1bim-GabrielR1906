bc56156c107d3874f6028f1c071ec2a0e0578fac
Gabriel Rojas <rgrojas4@utpl.edu.ec>
2022-11-30 16:30:19 -0500

    Taller final

---
+++ problema06/sol_python/problema06.py
@@ -0,0 +1,10 @@
+cadena_final = ""
+contador = 1
+numerador = 1
+suma = 0
+while contador <= 100:
+    suma = suma + float(numerador) / contador
+    cadena_final = "%s+ %d/%d " % (cadena_final, numerador, contador)
+    contador = contador + 1
+cadena_final = "%s\nSuma Total: %d\n" % (cadena_final, suma)
+print(cadena_final)
